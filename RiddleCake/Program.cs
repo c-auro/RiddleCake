c98c24cc29a131c32f7a4129b3a0e0fe5836549b
MechaKoopa <MechaKoopa@DESKTOP-PFBSFRN>
2016-08-13 03:19:33 -0700

    Add project files.

---
+++ RiddleCake/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSCore;
+using CSCore.SoundOut;
+using CSCore.Codecs;
+using System.Threading;
+
+namespace RiddleCake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program.PlayASound(@"C: \Users\MechaKoopa\Desktop\piano_note_20160812.mp3");
+            Program.PlayASound(@"C:\Users\MechaKoopa\Desktop\ding20160812.mp3"); 
+            
+        }
+
+        public static void PlayASound(string file)
+        {
+            //Contains the sound to play
+            using (IWaveSource soundSource = GetSoundSource(file))
+            {
+                //SoundOut implementation which plays the sound
+                using (ISoundOut soundOut = GetSoundOut())
+                {
+                    //Tell the SoundOut which sound it has to play
+                    soundOut.Initialize(soundSource);
+                    //Play the sound
+                    soundOut.Play();
+
+                    Thread.Sleep(2000);
+
+                    //Stop the playback
+                    soundOut.Stop();
+
+                }
+            }
+        }
+
+        private static ISoundOut GetSoundOut()
+        {
+            if (WasapiOut.IsSupportedOnCurrentPlatform)
+                return new WasapiOut();
+            else
+                return new DirectSoundOut();
+        }
+
+        private static IWaveSource GetSoundSource(string file)
+        {
+            //return any source ... in this example, we'll just play a mp3 file
+            return CodecFactory.Instance.GetCodec(file);
+        }
+    }
+}
---
+++ RiddleCake/Theory.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiddleCake.Theory
+{
+    class KeyConfig
+    {
+        enum minor
+        {
+            
+        }
+    }
+    class KeySignature
+    {
+        private int clef;
+        private char key;
+        private Note[][] scale; 
+        //integer representation of each type of scale
+        //use offsets to determine octave if threaded mp3 octaves, or integer offesets for literal frequency
+        //the key signature is determined per measure, so at the beginning of each measure, all of the scales 
+        //will be calculated if the key signature changes, this allows quick 'fingering' such as 135 or i-iii-v or some similar notation
+        //user can input a type of scale and the numbers or a default of major
+
+        public KeySignature(char k)
+        {
+            clef = 4; 
+            key = k;
+            switch (k)
+            {
+                //system could instead be based on the number of steps for major and minor chords
+                //but the scale would still be determined at that point
+                case 'A': GetScale('A');  break; 
+                case 'a': break;
+                case 'B': break;
+                case 'b': GetScale('b');  break;
+                default: break; 
+            }
+        }
+
+        private Note[][] GetScale(char k)
+        {
+           
+            return new Note[5][6];
+        }
+    }
+    class TimeSignature
+    {
+        private int beatsPerMeasure;
+        private int tempo; 
+        //beats per measure 
+        //note that gets one beat
+        //need to create an algorithm to calculate this
+    }
+    class Note
+    {
+        private int duration;
+        private int pitch;
+        private char letter; 
+    }
+    class Measure
+    {
+        public Measure(TimeSignature t, KeySignature k, Note[] n)
+        {
+
+        }
+    }
+    class Score
+    {
+        public Score(Measure[] m)
+        {
+            
+        }
+    }
+
+}
